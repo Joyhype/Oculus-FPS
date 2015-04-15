af7eeffeb82b6b2d7c5f73cbda7aaa018bb67807
Matt Stenquist <hello@mattmade.it>
2015-04-15 07:02:02 +0200

    Added in the Quest Script

---
+++ OculusSandbox/Assets/Scripts/Quests.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class Quests : MonoBehaviour {
+
+	public bool playerDoingQuest = false;
+	public bool questCompleted = false; 
+	
+	public _Quests getQuest;
+    
+    public enum _Quests {
+        None,
+        Cactus,
+        Bison
+    }
+  
+    void CheckForQuest() {
+        if (getQuest == _Quests.Bison) {
+            playerDoingQuest = true; 
+        }
+        
+        else if (getQuest == _Quests.Cactus) {
+        	playerDoingQuest = true; 
+        }
+
+        else if (getQuest == _Quests.None) {
+        	Debug.Log("Quests are available!");
+        	playerDoingQuest = false; 
+        }
+    }
+
+	void InQuest() {
+		if (playerDoingQuest == true) {
+			Debug.Log("Player is Doing a quest");
+		} else if (playerDoingQuest == false) {
+			Debug.Log("Player is Not Doing A Quest");
+		}
+
+	} 
+
+	void Update() {
+		InQuest(); 
+		CheckForQuest();
+		isQuestComplete();
+	}
+
+	void isQuestComplete() {
+		if (questCompleted == true) {
+			Debug.Log("Player has completed a quest");
+
+			//Do save data 
+			//Mark Quest Complete
+		}
+	}
+}
