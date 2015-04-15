2cdf2400e62355559eb7f5bc546fb83e56ea7e4e
Matt Stenquist <hello@mattmade.it>
2015-04-15 03:09:02 +0200

    Updated Assets:
    Added wagon, tweaked some things around, placed in new clouds.

---
+++ OculusSandbox/Assets/Scripts/FadeInObjects.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInObjects : MonoBehaviour {
+
+	void Start () {
+		iTween.FadeTo(gameObject, iTween.Hash ("alpha", 0, "time", 8));
+	}
+}
