4c749fddb09058a06e12d40d1359abc848d9e5b6
Matt Stenquist <hello@mattmade.it>
2015-04-16 05:18:48 +0200

    Added fire pit and nighttime scene!
    Cheeaaaahhhh boooyyyy

--- OculusSandbox/Assets/Scripts/RemoveBullets.cs
+++ OculusSandbox/Assets/Scripts/RemoveBullets.cs
@@ -5,7 +5,7 @@ public class RemoveBullets : MonoBehaviour {
 
 
 	IEnumerator Start() {
-		yield return new WaitForSeconds(3);
+		yield return new WaitForSeconds(1.5f);
 		Destroy(gameObject);
 	}
 }
