15af707b58de63717a6877f7a0ed269e2e08c743
Matt Stenquist <hello@mattmade.it>
2015-04-28 14:55:33 +0200

    Revert "Revert "Added Codebase Project""
    This reverts commit 49dd64c6e5bc1e3e16a6894392f8a85da995ee1a.

---
+++ Codebase/Assets/Code/Pippa.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pippa : MonoBehaviour {
+
+	public GameObject BulletPrefab;
+	public GameObject MuzzleFlashPrefab;
+
+	public GameObject GunTip;
+
+	public float ShotDelay = 0.5f;
+	public float ReloadDelay = 2.5f;
+
+	public float GunSwayDistance = 0.02f;
+	public float RecoilDistance = 0.15f;
+
+	private float reloadTimer = 0;
+	private float shotTimer = 0;
+	private int currentAmmo = 3;
+
+	// Use this for initialization
+	void Start() {
+		if ( BulletPrefab == null || MuzzleFlashPrefab == null || GunTip == null ) {
+			Debug.LogError( "Please put in the prefabs and the gun tip" );
+			Destroy( gameObject );
+		}
+
+		iTween.MoveBy( gameObject,
+			iTween.Hash( "y", -GunSwayDistance, "time", 2f,
+			"looptype", iTween.LoopType.pingPong,
+			"easetype", iTween.EaseType.easeInOutBack ) );
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if ( reloadTimer > 0 ) {
+			reloadTimer -= Time.deltaTime;
+		} else {
+			if ( shotTimer > 0 ) {
+				shotTimer -= Time.deltaTime;
+			} else {
+				if ( Input.GetMouseButtonDown( 0 ) ) {
+					Instantiate( BulletPrefab, GunTip.transform.position, transform.rotation );
+					Instantiate( MuzzleFlashPrefab, GunTip.transform.position, transform.rotation );
+
+					currentAmmo--;
+
+					if ( currentAmmo == 0 ) {
+						reloadTimer = ReloadDelay;
+						currentAmmo = 3;
+					} else {
+						shotTimer = ShotDelay;
+					}
+
+					iTween.MoveBy( gameObject,
+						iTween.Hash( "name", "recoilOne", "z", -0.15f, "time", 0.1f,
+						"easetype", iTween.EaseType.easeOutElastic ) );
+					iTween.MoveBy( gameObject,
+						iTween.Hash( "name", "recoilTwo", "z", 0.15f, "time", 0.1f, "delay", 0.11f,
+						"easetype", iTween.EaseType.linear ) );
+				}
+			}
+		}
+	}
+}
---
+++ Codebase/Assets/Code/Reticle.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class Reticle : MonoBehaviour {
+
+	public GameObject Gazer;
+	public float HighlightDistance = 10;
+	public float GazeDistance = 5;
+	public float GazeTime = 1;
+
+	private int gazeLayer;
+	private bool ignoreGaze = false;
+	private GameObject lastGazed = null;
+
+	// Use this for initialization
+	void Start() {
+		gazeLayer = LayerMask.GetMask( "Interactable" );
+	}
+
+	// Update is called once per frame
+	void Update() {
+		RaycastHit info;
+
+		Debug.DrawRay( transform.position, transform.forward, Color.red );
+
+		if ( Physics.Raycast( transform.position, transform.forward, out info, HighlightDistance, gazeLayer ) ) {
+			EnableGazeReticle();
+
+			var gazed = info.collider.gameObject;
+			if ( gazed != lastGazed ) {
+				ignoreGaze = false;
+				ResetScale();
+
+				if ( lastGazed != null ) {
+					lastGazed.BroadcastMessage( "OnLostGaze", SendMessageOptions.DontRequireReceiver );
+				}
+
+				lastGazed = gazed;
+				lastGazed.BroadcastMessage( "OnGaze", SendMessageOptions.DontRequireReceiver );
+			} else {
+				if ( info.distance > GazeDistance ) {
+					ignoreGaze = false;
+					ResetScale();
+					return;
+				}
+
+				if ( ignoreGaze ) {
+					ResetScale();
+					return;
+				}
+
+				var scale = Mathf.Min( Gazer.transform.localScale.x + ( Time.deltaTime * ( 1 / GazeTime ) ), 1 );
+				Gazer.transform.localScale = new Vector3( scale, 1, scale );
+
+				if ( scale == 1 ) {
+					ignoreGaze = true;
+					lastGazed.BroadcastMessage( "OnGazed", SendMessageOptions.RequireReceiver );
+				}
+			}
+		} else {
+			ignoreGaze = false;
+
+			if ( lastGazed != null ) {
+				lastGazed.BroadcastMessage( "OnLostGaze", SendMessageOptions.DontRequireReceiver );
+				lastGazed = null;
+			}
+
+			if ( ResetScale() ) {
+				DisableGazeReticle();
+			}
+		}
+	}
+
+	private void EnableGazeReticle() {
+		Gazer.SetActive( true );
+	}
+
+	private void DisableGazeReticle() {
+		Gazer.transform.localScale = new Vector3( 0.2f, 1, 0.2f );
+		Gazer.SetActive( false );
+	}
+
+	private bool ResetScale() {
+		var scale = Mathf.Max( Gazer.transform.localScale.x - ( Time.deltaTime * 5f ), 0.2f );
+		Gazer.transform.localScale = new Vector3( scale, 1, scale );
+
+		return scale == 0.2f;
+	}
+}
