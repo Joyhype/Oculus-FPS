521204597f66038fd780d70cf88349bf657b0b2c
Matt Stenquist <hello@mattmade.it>
2015-04-28 14:56:36 +0200

    Revert 15af707..3f8c2a6
    This rolls back to commit 15af707b58de63717a6877f7a0ed269e2e08c743.

---
+++ Codebase/Assets/Code/GameObjectEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class GameObjectEventArgs : EventArgs {
+
+	public Transform @Transform;
+	public Collider @Collider;
+	public Collider2D @Collider2D;
+	public Renderer @Renderer;
+	public Rigidbody @Rigidbody;
+	public Rigidbody2D @Rigidbody2D;
+
+	public GameObjectEventArgs( GameObject gameObject ) : base() {
+		Transform = gameObject.transform;
+		Collider2D = gameObject.GetComponent<Collider2D>();
+		Collider = gameObject.GetComponent<Collider>();
+		Renderer = gameObject.GetComponent<Renderer>();
+		Rigidbody = gameObject.GetComponent<Rigidbody>();
+		Rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+}
