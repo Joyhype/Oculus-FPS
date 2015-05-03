45030b86cf6c686cede81b7f39a83832543dc74d
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-05-03 15:38:58 +0200

    Updated codebase with new plugins, also added proper bison movement, or any movement for that matter

---
+++ Codebase/Assets/InputTester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void OnGUI() {
+		GUI.Label( new Rect( 125, 125, 200, 20 ), string.Format( "Position: {0}", Input.mousePosition ) );
+		GUI.Label( new Rect( 125, 145, 200, 20 ), string.Format( "Mouse button 0: {0}", Input.GetMouseButton( 0 ) ) );
+		GUI.Label( new Rect( 125, 165, 200, 20 ), string.Format( "Mouse button 1: {0}", Input.GetMouseButton( 1 ) ) );
+	}
+}
