6a5a2342f2075fd8cbdcfacef069ce7cad18551a
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-05-03 21:38:34 +0200

    Added code

---
+++ OculusSandbox/Assets/Code/LootGrabber.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LootGrabber : MonoBehaviour {
+
+	void OnTriggerEnter( Collider other ) {
+		if ( other.gameObject.layer == LayerMask.NameToLayer( "Collectible" ) ) {
+			MasterOfKnowledge.ItemPicked( other.gameObject.tag );
+		}
+
+		other.gameObject.BroadcastMessage( "OnPickup" );
+	}
+}
