e23045f27ec212acba22423decf422ea12b2a76b
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-05-05 00:08:21 +0200

    Trying to build a scene

---
+++ Chuck/Assets/Code/KillCollectQuest.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KillCollectQuest : BaseQuest {
+
+	public string[] CollectTags;
+	public string[] KillTags;
+
+	public int[] CollectAmounts;
+	public int[] KillAmounts;
+
+	protected Dictionary<string, int> collectObjects = new Dictionary<string, int>();
+	protected Dictionary<string, int> killObjects = new Dictionary<string, int>();
+
+	// Use this for initialization
+	void Start() {
+		foreach ( var item in CollectTags ) {
+			collectObjects.Add( item, 0 );
+		}
+
+		foreach ( var item in KillTags ) {
+			killObjects.Add( item, 0 );
+		}
+	}
+
+	protected void CheckTag( string tag ) {
+		if ( IsActive ) {
+			if ( collectObjects.ContainsKey( tag ) ) {
+				collectObjects[tag]++;
+				CheckConditions();
+			}
+
+			if ( killObjects.ContainsKey( tag ) ) {
+				killObjects[tag]++;
+				CheckConditions();
+			}
+		}
+	}
+
+	private void CheckConditions() {
+		bool completedAll = true;
+
+		for ( int i = 0; i < CollectTags.Length; i++ ) {
+			if ( killObjects[CollectTags[i]] < KillAmounts[i] ) {
+				completedAll = false;
+				break;
+			}
+		}
+		
+		for ( int i = 0; i < KillTags.Length; i++ ) {
+			if ( killObjects[KillTags[i]] < KillAmounts[i] ) {
+				completedAll = false;
+				break;
+			}
+		}
+
+		if ( completedAll ) {
+			FinishQuest();
+		}
+	}
+}
---
+++ Chuck/Assets/Code/QuestPocket.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestPocket : MonoBehaviour {
+
+	private BaseQuest activeQuest;
+
+	// Use this for initialization
+	void Start () {
+		MasterOfKnowledge.OnActivateQuest += MasterOfKnowledge_OnActivateQuest;
+		MasterOfKnowledge.OnDeliverQuest += MasterOfKnowledge_OnDeliverQuest;
+	}
+
+	private void MasterOfKnowledge_OnDeliverQuest( object sender, QuestEventArgs e ) {
+		
+	}
+
+	private void MasterOfKnowledge_OnActivateQuest( object sender, QuestEventArgs e ) {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
